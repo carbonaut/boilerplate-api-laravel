b2759eb385a9e21a15e34597ddcb8ce9a4b7046f
André <andrefedalto@gmail.com>
2024-06-10 17:14:00 -0300

    refactor(api): whitelist endpoints that are available during maintenance

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,11 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->preventRequestsDuringMaintenance(except: [
+            'auth/login',
+            'maintenance/up',
+            'maintenance/down',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
     })->create()
