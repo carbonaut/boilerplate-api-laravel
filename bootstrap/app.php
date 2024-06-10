7a93fd32e1cdad45e4a4f1c8718558e6b1ebbba3
André <andrefedalto@gmail.com>
2024-06-10 15:03:25 -0300

    refactor(router): update route configuration to the new Laravel 11 schema

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,31 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        using: function () {
+            $domain = config('app.domain');
+            assert(is_string($domain));
+
+            Route::middleware(['api', 'throttle:api'])
+                ->domain('api.' . $domain)
+                ->group(base_path('routes/api.php'))
+            ;
+
+            Route::middleware('web')
+                ->domain('www.' . $domain)
+                ->group(base_path('routes/web.php'))
+            ;
+
+            Route::middleware('web')
+                ->domain($domain)
+                ->group(base_path('routes/web.php'))
+            ;
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
     })
